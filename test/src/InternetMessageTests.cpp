656dcb8b45642e6008a1d121495966483e327a00
shaqsnake <shaqsnake@gmail.com>
2019-07-25 09:39:47 +0800

    First Commit.
    * Add cmake scripts, make project complie successfully.
    * Add first unittest.
    * Add InternetMessage header and source file, but they are blank for now.

---
+++ test/src/InternetMessageTests.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 shaqsnake. All rights reserved.
+/**
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-07-25 09:29:37
+ * @LastEditTime: 2019-07-25 09:35:05
+ * @Description: 
+ */
+#include <gtest/gtest.h>
+
+TEST(InternetMessageTests, Placeholder) {
+    ASSERT_EQ(42, 6 * 7);
+}
